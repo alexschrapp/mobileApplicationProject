00bfcb811ed8ea089ef76f50fa616cdb1b3b86a9
Tobias Tögel <71888952+TobiTgl@users.noreply.github.com>
2021-04-04 16:14:31 +0300

    Bottom Navigation Bar

--- RandomRecipe/app/src/main/java/com/group2/project/MainActivity.kt
+++ RandomRecipe/app/src/main/java/com/group2/project/MainActivity.kt
@@ -2,10 +2,53 @@ package com.group2.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_expirytracker -> {
+                toolbar.title = "Expiry Tracker"
+                val songsFragment = ExpiryTrackerFragment.newInstance()
+                openFragment(songsFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_home -> {
+                toolbar.title = "Home"
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_recipes -> {
+                toolbar.title = "Recipes"
+                val recipesFragment = RecipesFragment.newInstance()
+                openFragment(recipesFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
